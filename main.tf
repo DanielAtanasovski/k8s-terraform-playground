67672c6b3e02e641b58a6905a347253989332a99
Daniel Atanasovski <dtatanasovski@gmail.com>
2023-09-17 23:25:28 +1000

    feat: Add terraform and k8s config

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    kind = {
+      source  = "tehcyx/kind"
+      version = "~> 0.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+}
+
+provider "kind" {
+}
+
+provider "kubernetes" {
+  config_path = pathexpand(var.cluster_config_path)
+}
+
+provider "helm" {
+  config_path = pathexpand(var.cluster_config_path)
+}
+
+resource "kind_cluster" "default" {
+  name           = "test-cluster"
+  wait_for_ready = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+
+    node {
+      role = "worker"
+    }
+
+    node {
+      role = "worker"
+    }
+  }
+}
